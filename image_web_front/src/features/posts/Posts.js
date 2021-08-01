74af9a38f25b7592bb851fa1dcb98700e3aaafa3
Seoungwoo Baek <br1992ay@gmail.com>
2021-08-02 01:54:45 +0900

    REFACTOR : css refactoring
    반응형 웹 수정

--- image_web_front/src/features/posts/Posts.js
+++ image_web_front/src/features/posts/Posts.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { postsAction } from './postSlice';
 import PhotoList from './PhotoList';
+import '../../scss/Posts/Post.scss';
 //import More from './More';
 
 const Posts = () => {
 	const current_page = useSelector((state) => state.posts.current_page);
 	const dispatch = useDispatch();
 	const pageItemNumber = 9;
-
-
 
 	const onSetCurrentPage = ( _page ) => {
 		_page++;
@@ -26,8 +25,8 @@ const Posts = () => {
 	}
 
 	return (
-		<div>
-			<div className="PhotoContainer">
+		<div className="Post-template">
+			<div className="container">
 				<PhotoList onSetCurrentPage={onSetCurrentPage}
 							current_page={current_page}/>
 				{/*<More onSetCurrentPage={onSetCurrentPage}
