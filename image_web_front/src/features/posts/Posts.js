0ae460d9c8d269bc2d3048d6c2f223473ec85cca
Seoungwoo Baek <br1992ay@gmail.com>
2021-08-05 13:57:24 +0900

    FEAT : Logout function
    로그아웃 기능 추가

--- image_web_front/src/features/posts/Posts.js
+++ image_web_front/src/features/posts/Posts.js
@@ -3,13 +3,14 @@ import { useSelector, useDispatch } from 'react-redux';
 import { postsAction } from './postSlice';
 import PhotoList from './PhotoList';
 import '../../scss/Posts/Post.scss';
-import { setCookie } from '../../util/CookieUtil';
+import { getCookie, setCookie } from '../../util/CookieUtil';
 //import More from './More';
 
 const Posts = () => {
 	const current_page = useSelector((state) => state.posts.current_page);
 	const dispatch = useDispatch();
 	const pageItemNumber = 9;
+	const loginData = useSelector((state) => state.users);
 
 	const onSetCurrentPage = ( _page ) => {
 		_page++;
@@ -27,12 +28,18 @@ const Posts = () => {
 
 	const onClick = () => {
 		setCookie("loginInfo",null, 1);
-		console.log("setCookie");
+		console.log("setCookie null");
+	}
+
+	const onData = () => {
+		console.log("login stroe Data : ",loginData);
+		console.log("Cookile Data : ", getCookie("loginInfo"));
 	}
 
 	return (
 		<div className="Post-template">
-			<div onClick={onClick}>data</div>
+			<div onClick={onClick}>set Cookie null</div>
+			<div onClick={onData}>login Data</div>
 			<div className="container">
 				<PhotoList onSetCurrentPage={onSetCurrentPage}
 							current_page={current_page}/>
